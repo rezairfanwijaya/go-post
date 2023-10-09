f8727d5b7eede87d0e37f8fb294b193cad6d66ec
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-10-09 13:18:49 +0700

    test: completed unit test user handlers

--- internal/handler/user_test.go
+++ internal/handler/user_test.go
@@ -10,6 +10,7 @@ import (
 	postmock "go-post/internal/post/mocks"
 	"go-post/internal/user"
 	usermock "go-post/internal/user/mocks"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -136,7 +137,6 @@ func TestLoginSuccess(t *testing.T) {
 		r.POST("/users/login", h.Login)
 		r.ServeHTTP(rec, req)
 		assert.Equal(t, testCase.ExpectedCode, rec.Code)
-
 	})
 }
 
@@ -152,7 +152,7 @@ func TestLoginFailed(t *testing.T) {
 		Input        user.InputUserLogin
 		ExpectedCode int
 	}{
-		Name: "success",
+		Name: "failed",
 		Input: user.InputUserLogin{
 			Email:    "test@gmail.com",
 			Password: "12345678",
@@ -173,7 +173,7 @@ func TestLoginFailed(t *testing.T) {
 		inputJson, err := json.Marshal(testCase.Input)
 		assert.NoError(t, err)
 
-		uInteractor.On("GetUserByEmail", testCase.Input.Email).Return(u, http.StatusOK, nil)
+		uInteractor.On("GetUserByEmail", testCase.Input.Email).Return(u, http.StatusBadRequest, errors.New("failed"))
 		req := httptest.NewRequest(http.MethodPost, "/users/login", bytes.NewBuffer(inputJson))
 		req.Header.Set("Content-Type", "application/json")
 		rec := httptest.NewRecorder()
@@ -181,6 +181,147 @@ func TestLoginFailed(t *testing.T) {
 		r.POST("/users/login", h.Login)
 		r.ServeHTTP(rec, req)
 		assert.Equal(t, testCase.ExpectedCode, rec.Code)
+	})
+}
 
+func TestGetUserWithPostsSuccess(t *testing.T) {
+	pInteractor := postmock.NewInteractor(t)
+	uInteractor := usermock.NewInteractor(t)
+	auth := auth.NewAuthService()
+	h := handler.NewUserHandler(pInteractor, uInteractor, auth)
+
+	testCase := struct {
+		Name         string
+		UserId       int
+		ExpectedCode int
+	}{
+		Name:         "success",
+		UserId:       1,
+		ExpectedCode: http.StatusOK,
+	}
+
+	posts := []post.Post{
+		{
+			Id:      1,
+			UserId:  1,
+			Title:   "test 1",
+			Content: "test 1",
+		},
+		{
+			Id:      2,
+			UserId:  1,
+			Title:   "test 2",
+			Content: "test 2",
+		},
+	}
+
+	user := user.User{
+		Id:       1,
+		Email:    "test@gmail.com",
+		Password: "123456789",
+	}
+
+	t.Run(testCase.Name, func(t *testing.T) {
+		gin.SetMode(gin.TestMode)
+		r := gin.Default()
+
+		pInteractor.On("GetPostByUserId", testCase.UserId).Return(posts, http.StatusOK, nil)
+		uInteractor.On("GetUserById", testCase.UserId).Return(user, http.StatusOK, nil)
+
+		req := httptest.NewRequest(http.MethodGet, "/users/posts", nil)
+		rec := httptest.NewRecorder()
+
+		r.Use(func(ctx *gin.Context) {
+			ctx.Set("user", 1)
+		})
+
+		r.GET("/users/posts", h.GetUserWithPosts)
+
+		r.ServeHTTP(rec, req)
+	})
+}
+
+func TestGetUserWithPostsFailedFindUser(t *testing.T) {
+	pInteractor := postmock.NewInteractor(t)
+	uInteractor := usermock.NewInteractor(t)
+	auth := auth.NewAuthService()
+	h := handler.NewUserHandler(pInteractor, uInteractor, auth)
+
+	testCase := struct {
+		Name         string
+		UserId       int
+		ExpectedCode int
+	}{
+		Name:         "failed get user",
+		UserId:       1,
+		ExpectedCode: http.StatusOK,
+	}
+
+	posts := []post.Post{
+		{
+			Id:      1,
+			UserId:  1,
+			Title:   "test 1",
+			Content: "test 1",
+		},
+		{
+			Id:      2,
+			UserId:  1,
+			Title:   "test 2",
+			Content: "test 2",
+		},
+	}
+
+	t.Run(testCase.Name, func(t *testing.T) {
+		gin.SetMode(gin.TestMode)
+		r := gin.Default()
+
+		pInteractor.On("GetPostByUserId", testCase.UserId).Return(posts, http.StatusOK, nil)
+		uInteractor.On("GetUserById", testCase.UserId).Return(user.User{}, http.StatusInternalServerError, errors.New("failed"))
+
+		req := httptest.NewRequest(http.MethodGet, "/users/posts", nil)
+		rec := httptest.NewRecorder()
+
+		r.Use(func(ctx *gin.Context) {
+			ctx.Set("user", 1)
+		})
+
+		r.GET("/users/posts", h.GetUserWithPosts)
+		log.Println(rec)
+		r.ServeHTTP(rec, req)
+	})
+}
+
+func TestGetUserWithPostsFailedFindPosts(t *testing.T) {
+	pInteractor := postmock.NewInteractor(t)
+	uInteractor := usermock.NewInteractor(t)
+	auth := auth.NewAuthService()
+	h := handler.NewUserHandler(pInteractor, uInteractor, auth)
+
+	testCase := struct {
+		Name         string
+		UserId       int
+		ExpectedCode int
+	}{
+		Name:         "failed get posts",
+		UserId:       1,
+		ExpectedCode: http.StatusOK,
+	}
+
+	t.Run(testCase.Name, func(t *testing.T) {
+		gin.SetMode(gin.TestMode)
+		r := gin.Default()
+
+		pInteractor.On("GetPostByUserId", testCase.UserId).Return([]post.Post{}, http.StatusInternalServerError, errors.New("failed"))
+
+		req := httptest.NewRequest(http.MethodGet, "/users/posts", nil)
+		rec := httptest.NewRecorder()
+
+		r.Use(func(ctx *gin.Context) {
+			ctx.Set("user", 1)
+		})
+
+		r.GET("/users/posts", h.GetUserWithPosts)
+		r.ServeHTTP(rec, req)
 	})
 }
