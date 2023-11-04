21fa82e0e5ada700c6f5bfb2726d3e274a9b75fb
Reza Irfan Wijaya <reza.irfan@matchmade.io>
2023-11-04 14:47:50 +0700

    feat: update error handler user hanlder with user domain

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"go-post/internal/auth"
 	"go-post/internal/helper"
 	"go-post/internal/post"
@@ -52,14 +53,14 @@ func (h *userHandler) SignUp(c *gin.Context) {
 	newUser.Email = input.Email
 	newUser.Password = string(passwordHashed)
 
-	user, err := h.userInteractor.CreateUser(newUser)
+	u, err := h.userInteractor.CreateUser(newUser)
 	if err != nil {
 		log.Printf("failed create user, err: %s", err)
 		helper.GenerateResponseAPI(http.StatusInternalServerError, "error", "Failed create user", c, false)
 		return
 	}
 
-	helper.GenerateResponseAPI(http.StatusOK, "success", user, c, false)
+	helper.GenerateResponseAPI(http.StatusOK, "success", u, c, false)
 }
 
 func (h *userHandler) Login(c *gin.Context) {
@@ -74,8 +75,13 @@ func (h *userHandler) Login(c *gin.Context) {
 
 	u, err := h.userInteractor.GetUserByEmail(input.Email)
 	if err != nil {
-		log.Printf("failed to get user by email, email: %s, err: %s", input.Email, err)
-		helper.GenerateResponseAPI(http.StatusNotFound, "error", "User not found", c, false)
+		if errors.Is(err, user.ErrorUserNotFound) {
+			log.Printf("failed to get user by email, email: %s, err: %s", input.Email, err)
+			helper.GenerateResponseAPI(http.StatusNotFound, "error", "User not found", c, false)
+			return
+		}
+
+		helper.GenerateResponseAPI(http.StatusInternalServerError, "error", "Unknown error occurred", c, false)
 		return
 	}
 
@@ -98,21 +104,26 @@ func (h *userHandler) Login(c *gin.Context) {
 func (h *userHandler) GetUserWithPosts(c *gin.Context) {
 	userId := c.MustGet("user").(int)
 
-	posts, httpCode, err := h.postInteractor.GetPostByUserId(userId)
+	p, httpCode, err := h.postInteractor.GetPostByUserId(userId)
 	if err != nil {
 		helper.GenerateResponseAPI(httpCode, "error", err.Error(), c, false)
 		return
 	}
 
-	user, err := h.userInteractor.GetUserById(userId)
+	u, err := h.userInteractor.GetUserById(userId)
 	if err != nil {
-		helper.GenerateResponseAPI(0, "error", err.Error(), c, false)
+		if errors.Is(err, user.ErrorUserNotFound) {
+			log.Printf("failed to get user by id, id: %d, err: %s", userId, err)
+			helper.GenerateResponseAPI(http.StatusNotFound, "error", "User not found", c, false)
+			return
+		}
+		helper.GenerateResponseAPI(http.StatusInternalServerError, "error", "Unknown error occurred", c, false)
 		return
 	}
 
 	userWithPosts := response.UserWithPostsResponse{
-		User: user,
-		Post: posts,
+		User: u,
+		Post: p,
 	}
 
 	helper.GenerateResponseAPI(http.StatusOK, "success", userWithPosts, c, false)
